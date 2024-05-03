86a2fcf41161664f60098c45746ff7f9daca8ffb
Ranunculus6 <Ranunculus6>
2024-05-03 11:56:22 +0200

    fix torpedo

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,9 +78,11 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
-        firingSuccess = primaryTorpedoStore.fire(1);
-        firingSuccess = secondaryTorpedoStore.fire(1);
+        if (! primaryTorpedoStore.isEmpty()) 
+          firingSuccess = primaryTorpedoStore.fire(1);
+
+        if (! secondaryTorpedoStore.isEmpty())
+          firingSuccess = firingSuccess || secondaryTorpedoStore.fire(1);
 
         break;
     }
